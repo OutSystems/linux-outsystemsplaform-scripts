57936d60618f28cd4c90242fc0b341085242db91
ardoric <ardoric@gmail.com>
2015-03-03 21:47:51 +0000

    check all files
    instead of just jar files in lib

--- deployment/deploy_check.sh
+++ deployment/deploy_check.sh
@@ -6,6 +6,6 @@ ESPACES=$(ls $JBOSS_HOME/standalone/deployments/ | grep \\.war$ | grep -v custom
 
 
 for espace in $ESPACES; do
-	cd $JBOSS_HOME/standalone/deployments/$espace.war/WEB-INF/lib
-	echo $espace $(find -name '*.jar' | sort | xargs md5sum | md5sum | awk '{print $1}')
+	cd $JBOSS_HOME/standalone/deployments/$espace.war
+	echo $espace $(find -type f -or -type l | sort | xargs md5sum | md5sum | awk '{print $1}')
 done
